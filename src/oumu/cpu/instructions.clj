fb555d17a3c2bc2d8017e0863495ece027bcae63
Rafal Przywarski <rafal.przywarski@gmail.com>
2017-09-03 17:24:39 +0200

    decoding btw (r/m16 r16)

--- src/oumu/cpu/instructions.clj
+++ src/oumu/cpu/instructions.clj
@@ -463,6 +463,7 @@
    0xaa0f {::tag ::rsm, ::length 2}
    0xab0f {::tag ::bts, ::args [::r-or-m16 ::r16], ::length 3}
    0xb30f {::tag ::btr, ::args [::r-or-m16 ::r16], ::length 3}
+   0xbb0f {::tag ::btc, ::args [::r-or-m16 ::r16], ::length 3}
    0x8a0f {::tag ::jp, ::args [::rel16], ::length 2}
    0x8b0f {::tag ::jnp, ::args [::rel16], ::length 2}
    0x8c0f {::tag ::jl, ::args [::rel16], ::length 2}
